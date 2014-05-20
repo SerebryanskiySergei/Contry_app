0ef2a5e89993ed1f0d10a59b93c19af4e6ff0ba1
Official repository <serebryanskiysergei@gmail.com>
2014-05-20 11:59:59 +0400

    Добавлен класс страна (без рисовки)

---
+++ Countries/Countries/Country.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countries
+{
+    class Country:ICountry
+    {
+        private readonly string _name;
+
+        public Country(string name)
+        {
+            _name = name;
+        }
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
